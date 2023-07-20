e689c1fc0c34923b03494a4c3b9d2699d3d5446d
jonathancriado <jonathancriado93@gmail.com>
2023-07-19 21:15:55 -0300

    numero aleatorio para caracter aleatorio

--- 7-2023/004generadorDeContrasenias.js
+++ 7-2023/004generadorDeContrasenias.js
@@ -1,1 +1,15 @@
-console.log(2e3)
+/*
+ * Escribe un programa que sea capaz de generar contraseñas de forma aleatoria.
+ * Podrás configurar generar contraseñas con los siguientes parámetros:
+ * - Longitud: Entre 8 y 16.
+ * - Con o sin letras mayúsculas.
+ * - Con o sin números.
+ * - Con o sin símbolos.
+ * (Pudiendo combinar todos estos parámetros entre ellos)
+ */
+// 
+let numeroAleatorio = Math.random(Math.floor())*100;
+console.log(numeroAleatorio)
+
+let letra = String.fromCharCode(numeroAleatorio)
+console.log(letra)
