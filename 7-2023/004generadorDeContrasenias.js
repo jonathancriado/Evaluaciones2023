63a287bbea71dec7a2dae3537082b1a6a98845e8
jonathancriado <jonathancriado93@gmail.com>
2023-07-21 11:51:58 -0300

    genera una contraseña con una condicion

--- 7-2023/004generadorDeContrasenias.js
+++ 7-2023/004generadorDeContrasenias.js
@@ -7,14 +7,23 @@
  * - Con o sin símbolos.
  * (Pudiendo combinar todos estos parámetros entre ellos)
  */
-// 
+
+// Necesito que el valor random no sobrepase el largo del array 
+
 let numeroDeCaracteres = long => {
-    return long >= 8 && long <= 16 ? true : false
+    const listaDeCaracteres = ["A", "j", "2", "7", "#", "%"]
+    const listaContraseña = []
+    if (long >= 8 && long <= 16) {
+        while (listaContraseña.length < long) {
+            let posicionEnLaLista = Math.floor(Math.random() * listaDeCaracteres.length);
+            listaContraseña.push(listaDeCaracteres[posicionEnLaLista])
+    
+            console.log(listaContraseña.join(""))
+        }
+    } else {
+        console.log("Su contraseña debe contener entre 8 y 16 caracteres")
+    }
 }
-console.log(numeroDeCaracteres(5))
+numeroDeCaracteres(8)
 
-let numeroAleatorio = Math.floor(Math.random()*1000);
-console.log(numeroAleatorio)
 
-let letra = String.fromCharCode(numeroAleatorio)
-console.log(letra)
