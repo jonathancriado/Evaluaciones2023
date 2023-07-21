420c087a0c1e1a7e8944fadc4363423f9ab3576e
jonathancriado <jonathancriado93@gmail.com>
2023-07-21 17:14:10 -0300

    Numero par funciona

--- 7-2023/005primoFibonacciPar.js
+++ 7-2023/005primoFibonacciPar.js
@@ -5,23 +5,22 @@
  * - Con el número 7, nos dirá: "7 es primo, no es fibonacci y es impar"
  */
 // 0+1=1 / 1+1=2 / 1+2=3 / 2+3=5 / 3+5=8 / 5+8=13
-function fibonacci(numeroRandom) {
+fibonacci = numeroAProbar => {
     let numeroFibonacci2 = 1;
     let numeroFibonacci1 = 0;
     let nuevoNumeroFibonacci = 0;
-    const listaDeFibonacci = [0];
-    while (numeroRandom >= nuevoNumeroFibonacci) {
+    const listaDeFibonacci = [];
+    while (numeroAProbar >= nuevoNumeroFibonacci) {
         nuevoNumeroFibonacci = numeroFibonacci1 + numeroFibonacci2
         listaDeFibonacci.push(nuevoNumeroFibonacci)
         numeroFibonacci1 = numeroFibonacci2
         numeroFibonacci2 = nuevoNumeroFibonacci
     }
-    console.log(listaDeFibonacci)
-    console.log(numeroRandom)    
-    listaDeFibonacci.includes(numeroRandom) ? console.log("Eureka") : console.log("Puta vida")
+    listaDeFibonacci.includes(numeroAProbar) ? console.log("Es Fibonacci") : console.log("No es Fibonacci")
 }
-fibonacci(Math.floor(Math.random()*100))
+par = numeroAProbar => {
+    numeroAProbar % 2 == 0 ? console.log("Es par") : console.log("No es par")
+}
 
 
-// console.log(numeroRandom)
 
