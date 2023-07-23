bbdcb03e1e512b51da743674cb8a68310199599d
jonathancriado <jonathancriado93@gmail.com>
2023-07-23 19:08:32 -0300

    Resuelto funciona correctamente

--- 7-2023/005primoFibonacciPar.js
+++ 7-2023/005primoFibonacciPar.js
@@ -4,23 +4,42 @@
  * - Con el número 2, nos dirá: "2 es primo, fibonacci y es par"
  * - Con el número 7, nos dirá: "7 es primo, no es fibonacci y es impar"
  */
-// 0+1=1 / 1+1=2 / 1+2=3 / 2+3=5 / 3+5=8 / 5+8=13
-fibonacci = numeroAProbar => {
+let fibonacci = numero => {
     let numeroFibonacci2 = 1;
     let numeroFibonacci1 = 0;
     let nuevoNumeroFibonacci = 0;
     const listaDeFibonacci = [];
-    while (numeroAProbar >= nuevoNumeroFibonacci) {
+    while (numero >= nuevoNumeroFibonacci) {
         nuevoNumeroFibonacci = numeroFibonacci1 + numeroFibonacci2
         listaDeFibonacci.push(nuevoNumeroFibonacci)
         numeroFibonacci1 = numeroFibonacci2
         numeroFibonacci2 = nuevoNumeroFibonacci
     }
-    listaDeFibonacci.includes(numeroAProbar) ? console.log("Es Fibonacci") : console.log("No es Fibonacci")
+    if (listaDeFibonacci.includes(numero)) {
+        return "Fibonacci"
+    } else {
+        return "no es Fibonacci"
+    }
 }
-par = numeroAProbar => {
-    numeroAProbar % 2 == 0 ? console.log("Es par") : console.log("No es par")
+let par = numero => {
+    if (numero % 2 == 0) {
+        return "Es par"
+    } else {
+        return "es impar"
+    }
+}
+let primo = numero => {
+    if (numero < 1) {
+        return "no es primo"
+    }
+    for (let i = 2; i < numero; i++) {
+        if (numero % i == 0) return "no es primo"
+    }
+    return "es primo"
+}
+respuesta = numero => {
+    console.log(`${numero} ${primo(numero)}, ${fibonacci(numero)} y ${par(numero)}`)
 }
 
-
+respuesta(7)
 
