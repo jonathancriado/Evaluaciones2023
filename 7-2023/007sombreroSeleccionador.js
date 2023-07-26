122a977bee0c845361cfcd739a11a93ea255d5ed
jonathancriado <jonathancriado93@gmail.com>
2023-07-26 17:11:19 -0300

    primer commit ejercicio resuelto
    Hay que optimizarlo

---
+++ 7-2023/007sombreroSeleccionador.js
@@ -0,0 +1,116 @@
+const questionsAndAnswers = [
+    {
+        question: "¿Cómo te definirías?",
+        answer: [
+            ["1. Valiente", "gryffindor"],
+            ["2. Leal", "hufflepuff"],
+            ["3. Sabio", "ravenclaw"],
+            ["4. Ambicioso", "slytherin"]
+        ]
+    },
+    {
+        question: "¿Cuál es tu clase favorita?",
+        answer: [
+            ["1. Vuelo", "gryffindor"],
+            ["2. Pociones", "ravenclaw"],
+            ["3. Defensa contra las artes oscuras", "slytherin"],
+            ["4. Animales fantásticos", "hufflepuff"]
+        ]
+    },
+    {
+        question: "¿Dónde pasarías más tiempo?",
+        answer: [
+            ["1. Invernadero", "hufflepuff"],
+            ["2. Biblioteca", "ravenclaw"],
+            ["3. En la sala común", "slytherin"],
+            ["4. Explorando", "gryffindor"]
+        ]
+    },
+    {
+        question: "¿Cuál es tu color favorito?",
+        answer: [
+            ["1. Rojo", "gryffindor"],
+            ["2. Azul", "ravenclaw"],
+            ["3. Verde", "slytherin"],
+            ["4. Amarillo", "hufflepuff"]
+        ]
+    },
+    {
+        question: "¿Cuál es tu mascota?",
+        answer: [
+            ["1. Sapo", "ravenclaw"],
+            ["2. Lechuza", "gryffindor"],
+            ["3. Gato", "hufflepuff"],
+            ["4. Serpiente", "slytherin"]
+        ]
+    },
+];
+
+const houses = {
+    "ravenclaw": 0,
+    "gryffindor": 0,
+    "hufflepuff": 0,
+    "slytherin": 0,
+};
+
+/*
+ * Crea un programa que simule el comportamiento del sombrero selccionador del
+ * universo mágico de Harry Potter.
+ * - De ser posible realizará 5 preguntas (como mínimo) a través de la terminal.
+ * - Cada pregunta tendrá 4 respuestas posibles (también a selecciona una a través de terminal).
+ * - En función de las respuestas a las 5 preguntas deberás diseñar un algoritmo que
+ *   coloque al alumno en una de las 4 casas de Hogwarts (Gryffindor, Slytherin , Hufflepuff y Ravenclaw)
+ * - Ten en cuenta los rasgos de cada casa para hacer las preguntas y crear el algoritmo seleccionador.
+ *   Por ejemplo, en Slytherin se premia la ambición y la astucia.
+ */
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+const { Console } = require('node:console');
+const rl = readline.createInterface({ input, output });
+let preguntaNumero = 0;
+function preguntar() {
+    if (preguntaNumero < questionsAndAnswers.length) {
+        return preguntaYOpciones()
+    }
+    if (preguntaNumero >= questionsAndAnswers.length) {
+        return finalizar();
+    };
+}
+function preguntaYOpciones() {
+    let pregunta = questionsAndAnswers[preguntaNumero].question
+    console.log(pregunta)
+    let opciones = questionsAndAnswers[preguntaNumero].answer
+    opciones.forEach(elem => console.log(elem[0]))
+    rl.question("Seleccione una opcion: ", respuesta => {
+        if (Number(respuesta) > 0 && Number(respuesta) < 5) {
+            let posicionOpcionSeleccionada = questionsAndAnswers[preguntaNumero].answer[Number(respuesta) - 1][1];
+            if (preguntaNumero === 1) {
+                if (posicionOpcionSeleccionada === "ravenclaw") houses["ravenclaw"] += 5;
+                if (posicionOpcionSeleccionada === "gryffindor") houses["gryffindor"] += 5;
+                if (posicionOpcionSeleccionada === "hufflepuff") houses["hufflepuff"] += 5;
+                if (posicionOpcionSeleccionada === "slytherin") houses["slytherin"] += 5;
+                preguntaNumero++;
+                preguntar();
+            } else {
+                if (posicionOpcionSeleccionada === "ravenclaw") houses["ravenclaw"] += 3;
+                if (posicionOpcionSeleccionada === "gryffindor") houses["gryffindor"] += 3;
+                if (posicionOpcionSeleccionada === "hufflepuff") houses["hufflepuff"] += 3;
+                if (posicionOpcionSeleccionada === "slytherin") houses["slytherin"] += 3;
+                preguntaNumero++;
+                preguntar();
+            }
+        } else {
+            console.log("Tu respuesta tiene que ser 1, 2, 3 o 4")
+            preguntar()
+        };
+    });
+};
+function finalizar() {
+    let arrayHousesValues = Object.values(houses)
+    let numeroGanador = Math.max(...arrayHousesValues)
+    let arrayHousesKeys = Object.keys(houses)
+    let casaGanadora = arrayHousesKeys.find(key => houses[key] === numeroGanador)
+    console.log(`Te pondré en ¡¡${casaGanadora}!!`)
+    return rl.close();
+}
+preguntar()
