c78984fec0e7ace9deab43f54abfceca8f121f95
jonathancriado <jonathancriado93@gmail.com>
2023-08-01 10:06:46 -0300

    numero random 2 formas ok

--- 7-2023/008numeroAleatorio.js
+++ 7-2023/008numeroAleatorio.js
@@ -4,8 +4,11 @@
  *
  * Es más complicado de lo que parece...
  */
-let fecha = new Date()
-nuemroRandom = () => Math.floor(fecha.getMilliseconds()/10)
+
+nuemroRandom = () => {
+    let fecha = new Date()
+    return Math.floor(fecha.getMilliseconds()/10)
+}
 console.log(nuemroRandom())
 
 // NUMERO RANDOM USANDO LA MEMORIA RAM UTILIZADA
@@ -16,11 +19,14 @@ console.log(nuemroRandom())
 //     const memoriaLibre = os.freemem();
 //     const memoriaTotal = os.totalmem();
 //     const memoriaUtilizada = memoriaTotal - memoriaLibre;
-//     return memoriaUtilizada
+//     let result = 0;
+//     for (let i = 1; i <= 10000000000; i *= 10) {
+//         let numeroDecimal = (memoriaUtilizada / i) - Math.floor(memoriaUtilizada / i);
+//         let numeroRandom = numeroDecimal * 100
+//         if (numeroRandom > 0 && numeroRandom < 100) {
+//             return result = Math.floor(numeroRandom);
+//         }
+//     }
 // }
 
-// numeroPseudorandom = () => {
-//     const nuemroRandom = (obtenerMemoriaRamUtilizada() / 100000000) - Math.floor(obtenerMemoriaRamUtilizada() / 100000000);
-//     if (nuemroRandom > 0 && nuemroRandom < 100) return Math.floor(nuemroRandom * 100)
-// }
-// console.log('Numero pseudorandom', numeroPseudorandom());
+// console.log(obtenerMemoriaRamUtilizada());
---
+++ 7-2023/009cadenasDeTexto.js
@@ -0,0 +1,16 @@
+/*
+ * Crea 3 funciones, cada una encargada de detectar si una cadena de
+ * texto es un heterograma, un isograma o un pangrama.
+ * - Debes buscar la definición de cada uno de estos términos.
+*/
+
+// HETEROGRAMA : frase en la que las letras no se repiten
+
+// ISOGRAMA : frase en la que cada letra aparece el mismo numero de veces
+let isograma = frase => {
+    frase.split("")
+}
+console.log("aaabbbcccdddd") // false
+
+// PANGRAMA : frase en la que aparecen todas las letras de un alfabeto
+
