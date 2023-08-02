0d571d1c48a6c56c18879d59501bfbf1104195bd
jonathancriado <jonathancriado93@gmail.com>
2023-08-02 15:32:48 -0300

    isograma funciona LISTO

--- 7-2023/009cadenasDeTexto.js
+++ 7-2023/009cadenasDeTexto.js
@@ -21,19 +21,48 @@ let heterograma = frase => {
 }
 // ISOGRAMA : frase en la que cada letra aparece el mismo numero de veces
 let isograma = frase => {
+    const abecedario = {
+        "a": 0,
+        "b": 0,
+        "c": 0,
+        "d": 0,
+        "e": 0,
+        "f": 0,
+        "g": 0,
+        "h": 0,
+        "i": 0,
+        "j": 0,
+        "k": 0,
+        "l": 0,
+        "m": 0,
+        "n": 0,
+        "ñ": 0,
+        "o": 0,
+        "p": 0,
+        "q": 0,
+        "r": 0,
+        "s": 0,
+        "t": 0,
+        "u": 0,
+        "v": 0,
+        "w": 0,
+        "x": 0,
+        "y": 0,
+        "z": 0
+    }
     const letras = frase
         .toLowerCase()
         .replaceAll(/[^a-zA-ZáéíóúüÁÉÍÓÚñÑ]/g, "")
-        .split("");  
-    const cantidadDeCaracteres = []
+    const arrAbecedario = Object.keys(abecedario)
     for (let i = 0; i < letras.length; i++) {
-        let letra = letras[i]
-        let arrCaracInd = letras.filter(elem => elem === letra)
-        cantidadDeCaracteres.push(arrCaracInd.length)
+        for (let j = 0; j < arrAbecedario.length; j++) {
+            if (letras[i] === arrAbecedario[j]) abecedario[letras[i]] += 1
+        }
     }
-    let valorAConfirmar = cantidadDeCaracteres[0]
-    let respuesta = cantidadDeCaracteres.every(elem => elem === valorAConfirmar) ? "Es un isograma" : "No es un isograma"
-    return respuesta
+    const abecedarioValores = Object.values(abecedario)
+    cantidadDeRepeticiones = abecedarioValores.filter(elem => elem !== 0)
+    resultado = cantidadDeRepeticiones.every(elem => elem === cantidadDeRepeticiones[0]) ? "Es un isograma" : "No es isograma"
+    return resultado
 }
 // PANGRAMA : frase en la que aparecen todas las letras de un alfabeto
 let pangrama = frase => {
@@ -68,6 +97,6 @@ console.log(heterograma("luteranismo"))
 // console.log(pangrama("Jovencillo emponzoñado de whisky, ¡qué figurota exhibe!"))
 // console.log(pangrama("La cigüeña tocaba el saxofó,mñvwxyzkhn detrás del palenque paja."))
 
-// console.log(isograma(""))
-// console.log(isograma(""))
-// console.log(isograma(""))
+// console.log(isograma("aaa 2bb bccc"))
+// console.log(isograma("asf #gew r"))
+// console.log(isograma("avcbnja!"))
