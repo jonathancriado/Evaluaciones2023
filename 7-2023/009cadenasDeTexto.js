212c3276752a4d01e428752970a8ef16c44c2bf9
jonathancriado <jonathancriado93@gmail.com>
2023-08-03 11:54:36 -0300

    Todos Funcionan

--- 7-2023/009cadenasDeTexto.js
+++ 7-2023/009cadenasDeTexto.js
@@ -5,19 +5,26 @@
 */
 // HETEROGRAMA : frase en la que las letras no se repiten
 let heterograma = frase => {
-    const letras = frase.replaceAll(" ", "").toLowerCase().split("");
-    const cantidadDeCaracteres = []
-    for (let i = 0; i < letras.length; i++) {
-        let letra = letras[i]
-        let arrCaracInd = letras.filter(elem => elem === letra)
-        cantidadDeCaracteres.push(arrCaracInd.length)
+    const letrasSinTilde = {
+        "á": "a",
+        "é": "e",
+        "í": "i",
+        "ó": "o",
+        "ú": "u",
+        "ü": "u",
     }
-    let resultado = cantidadDeCaracteres.every(elem => elem === 1)
-    if (resultado) {
-        return "Es un heterograma"
-    } else {
-        return "No es un heterograma"
+    const arrSinEspaciosSignos = frase
+        .toLowerCase()
+        .replaceAll(/[^a-zA-ZáéíóúüÁÉÍÓÚñÑ]/g, "")
+        .split("")
+    const tilde = Object.keys(letrasSinTilde)
+    for (let i = 0; i < arrSinEspaciosSignos.length; i++) {
+        for (let j = 0; j < tilde.length; j++)
+            arrSinEspaciosSignos[i] === tilde[j] ? arrSinEspaciosSignos[i] = letrasSinTilde[tilde[j]] : arrSinEspaciosSignos[i]
     }
+    let arrSinDuplicados = [...new Set(arrSinEspaciosSignos)]
+    resultado = arrSinDuplicados.length === arrSinEspaciosSignos.length ? "Es un heterograma" : "No es un heterograma"
+    return resultado
 }
 // ISOGRAMA : frase en la que cada letra aparece el mismo numero de veces
 let isograma = frase => {
@@ -50,18 +57,33 @@ let isograma = frase => {
         "y": 0,
         "z": 0
     }
-    const letras = frase
+    const arrAbecedario = Object.keys(abecedario)
+    const letrasSinTilde = {
+        "á": "a",
+        "é": "e",
+        "í": "i",
+        "ó": "o",
+        "ú": "u",
+        "ü": "u",
+    }
+    const arrSinEspaciosSignos = frase
         .toLowerCase()
         .replaceAll(/[^a-zA-ZáéíóúüÁÉÍÓÚñÑ]/g, "")
-    const arrAbecedario = Object.keys(abecedario)
-    for (let i = 0; i < letras.length; i++) {
+        .split("")
+    const tilde = Object.keys(letrasSinTilde)
+    for (let i = 0; i < arrSinEspaciosSignos.length; i++) {
+        for (let j = 0; j < tilde.length; j++)
+            arrSinEspaciosSignos[i] === tilde[j] ? arrSinEspaciosSignos[i] = letrasSinTilde[tilde[j]] : arrSinEspaciosSignos[i]
+    }
+    // falta ver 
+    for (let i = 0; i < arrSinEspaciosSignos.length; i++) {
         for (let j = 0; j < arrAbecedario.length; j++) {
-            if (letras[i] === arrAbecedario[j]) abecedario[letras[i]] += 1
+            if (arrSinEspaciosSignos[i] === arrAbecedario[j]) abecedario[arrSinEspaciosSignos[i]] += 1
         }
     }
     const abecedarioValores = Object.values(abecedario)
     cantidadDeRepeticiones = abecedarioValores.filter(elem => elem !== 0)
-    resultado = cantidadDeRepeticiones.every(elem => elem === cantidadDeRepeticiones[0]) ? "Es un isograma" : "No es isograma"
+    resultado = cantidadDeRepeticiones.every(elem => elem === cantidadDeRepeticiones[0]) ? "Es un isograma" : "No es isograma";
     return resultado
 }
 // PANGRAMA : frase en la que aparecen todas las letras de un alfabeto
@@ -84,19 +106,18 @@ let pangrama = frase => {
             arrSinEspaciosSignos[i] === tilde[j] ? arrSinEspaciosSignos[i] = letrasSinTilde[tilde[j]] : arrSinEspaciosSignos[i]
     }
     let arrSinDuplicados = [...new Set(arrSinEspaciosSignos)]
-    console.log(arrSinDuplicados.sort())
     let resultado = arrSinDuplicados.length === 27 ? "Es pangrama" : "No es pangrama"
-    return resultado
+    return resultado;
 }
 
-console.log(heterograma("yuxtaponer"))
-console.log(heterograma("centrifugaado"))
-console.log(heterograma("luteranismo"))
+console.log(heterograma("yuxtap oner"))
+console.log(heterograma("ce nt#ri fugado"))
+console.log(heterograma("lut +eraa nis  mo"))
 
-// console.log(pangrama("El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña tocaba el saxofón detrás del palenque de paja"))
-// console.log(pangrama("Jovencillo emponzoñado de whisky, ¡qué figurota exhibe!"))
-// console.log(pangrama("La cigüeña tocaba el saxofó,mñvwxyzkhn detrás del palenque paja."))
+console.log(isograma("aÁa 2bb bccc"))
+console.log(isograma("asf #g,ew r"))
+console.log(isograma("avcbánj!"))
 
-// console.log(isograma("aaa 2bb bccc"))
-// console.log(isograma("asf #gew r"))
-// console.log(isograma("avcbnja!"))
+console.log(pangrama("El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña tocaba el saxofón detrás del palenque de paja"))
+console.log(pangrama("Jovencillo emponzoñado de whisky, ¡qué figurota exhibe!"))
+console.log(pangrama("La cigüeña tocaba el saxofón, detrás del palenque paja."))
