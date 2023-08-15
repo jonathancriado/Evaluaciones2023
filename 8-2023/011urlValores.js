8701584cde45a1d6fb90e868146fe71664074952
jonathancriado <jonathancriado93@gmail.com>
2023-08-15 12:05:08 -0300

    Falta ver el peor caso

--- 8-2023/011urlValores.js
+++ 8-2023/011urlValores.js
@@ -7,12 +7,13 @@
  */
 
 let parameters = url => {
-    let urlChange = url.replaceAll("&", "=")
-    const urlKeysAndValues = urlChange.split("=")
-    const urlValues = []
+    let urlChange = url.replaceAll("&", "=");
+    const urlKeysAndValues = urlChange.split("=");
+    const urlValues = [];
     for (let i = 1; i < urlKeysAndValues.length; i += 2) {
-        urlValues.push(urlKeysAndValues[i])
+        urlValues.push(urlKeysAndValues[i]);
     }
     return urlValues;
 }
 console.log(parameters("https://retosdeprogramacion.com?year=2023&challenge=0&name=jony"))
+console.log(parameters("https://retosdeprogramacion.com?year=2023&challenge=0&nombre=lucas"))
--- 8-2023/012friday13.js
+++ 8-2023/012friday13.js
@@ -3,8 +3,14 @@
  * - La función recibirá el mes y el año y retornará verdadero o falso.
  */
 let friday13 = (year, month) => {
-    const date = new Date(year, month, day = 13);
+    const date = new Date(Number(year), Number(month) - 1, day = 13);
     let result = date.getDay() === 5 ? true : false
     return result
 }
-console.log(friday13(2024, 11))
+console.log(friday13(2023, 3))
+console.log(friday13(2023, 1))
+console.log(friday13(2023, 13))
+console.log(friday13(-2023, 1))
+console.log(friday13(2023, "1"))
+console.log(friday13(2023, 11))
+console.log(friday13("Brais", "Moure"))
